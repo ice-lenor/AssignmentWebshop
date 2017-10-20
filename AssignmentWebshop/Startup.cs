3d6f868a3c6d8c37e2d43bceca51a372b1fe7f97
lenor <ice.lenor@gmail.com>
2017-10-20 20:07:23 +0200

    "Webshop" assignment from IReckonU: new MVC project from template

---
+++ AssignmentWebshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssignmentWebshop.Startup))]
+namespace AssignmentWebshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
